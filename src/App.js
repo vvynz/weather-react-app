475e2e6c24ee1c77b4b9c5854a63832a876b3286
Vivian Zhang <vvynz12@gmail.com>
2022-04-15 19:08:53 -0400

    Import React and rename app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import React from "react";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World</h1>
+        <h1>Weather App</h1>
        
       </header>
     </div>
