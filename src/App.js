15af3e2a2e469155fd04f9fef892c96614d89950
Vivian Zhang <vvynz12@gmail.com>
2022-04-15 19:13:34 -0400

    Import SearchEngine file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import SearchEngine from "./SearchEngine";
 import "./App.css";
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Weather App</h1>
-       
+        <SearchEngine />
       </header>
     </div>
   );
