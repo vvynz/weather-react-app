06e422ab73693834b8f07ea33da8bac10393cec5
Vivian Zhang <vvynz12@gmail.com>
2022-04-18 19:16:24 -0400

    Add conditional statements so when the hour or minutes are single digits, they'll be formatted with a 0

--- src/FormateDate.jsx
+++ src/FormateDate.jsx
@@ -6,8 +6,11 @@ export default function FormatDate(props) {
   const date = props.date;
   const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   const day = days[date.getDay()];
-  const hour = date.getHours();
-  const minutes = date.getMinutes();
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
+
+  if (hour < 10) hour = `0${hour}`;
+  if (minutes < 10) minutes = `0${minutes}`;
 
   return(
     <p>{day} {hour}:{minutes}</p>
