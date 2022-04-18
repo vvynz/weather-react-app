4aa4b4ddb8e18d2fe0ef37441f9386fc4f564c3a
Vivian Zhang <vvynz12@gmail.com>
2022-04-18 19:12:03 -0400

    Format the date to display the day, hour and minutes

--- src/FormateDate.jsx
+++ src/FormateDate.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
-export default function FormatDate() {
-  return "Tuesday, 6:13";
+export default function FormatDate(props) {
+  // console.log("Date props", props.date);
+
+  const date = props.date;
+  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const day = days[date.getDay()];
+  const hour = date.getHours();
+  const minutes = date.getMinutes();
+
+  return(
+    <p>{day} {hour}:{minutes}</p>
+  );
 }
