90eb5a5f6187c27d8f324e595aa5854672e65172
Vivian Zhang <vvynz12@gmail.com>
2022-04-19 19:41:35 -0400

    Replace precipitation and wind display info with icons

--- src/WeatherInfo.jsx
+++ src/WeatherInfo.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import FormatDate from "./FormateDate";
 import WeatherIcon from "./WeatherIcon";
+import ReactAnimatedWeather from "react-animated-weather";
 
 export default function WeatherInfo(props) {
   const data = props.data;
@@ -8,21 +9,37 @@ export default function WeatherInfo(props) {
   return (
     <div>
       <h1>{data.city}</h1>
-        <FormatDate date={data.date} />
-        <p>{data.description}</p>
-        <div className="row">
-          <div className="col-6">
-            <WeatherIcon icon={data.icon}/>
-            {Math.round(data.temperature)}°C
-          </div>
-          <div className="col-6">
-            <ul>
-              <li>Precipitation: {data.precipitation}%</li>
-              <li>Humidity: {data.humidity}%</li>
-              <li>wind: {data.wind}km/h</li>
-            </ul>
-          </div>
+      <FormatDate date={data.date} />
+      <p>{data.description}</p>
+      <div className="row">
+        <div className="col-6">
+          <WeatherIcon icon={data.icon} />
+          {Math.round(data.temperature)}°C
         </div>
+        <div className="col-6">
+          <ul>
+            <li>
+              <ReactAnimatedWeather
+                icon="SLEET"
+                color="GREY"
+                size={18}
+                animate={true}
+              />
+              : {data.precipitation}%
+            </li>
+            <li>Humidity: {data.humidity}%</li>
+            <li>
+              <ReactAnimatedWeather
+                icon="WIND"
+                color="GREY"
+                size={18}
+                animate={true}
+              />
+              : {data.wind}km/h
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
