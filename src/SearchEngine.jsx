ffd143a9ad1e46e48a80652d31f89e0c98ec2d7e
Vivian Zhang <vvynz12@gmail.com>
2022-04-19 18:31:51 -0400

    Pass weatherData as props down to the WeatherInfo component

--- src/SearchEngine.jsx
+++ src/SearchEngine.jsx
@@ -67,23 +67,7 @@ export default function SearchEngine(props) {
             </div>
           </div>
         </form>
-        <WeatherInfo />
-        <h1>{weatherData.city}</h1>
-        {/* <FormatDate date={weatherData.date} /> */}
-        <p>{weatherData.description}</p>
-        <div className="row">
-          <div className="col-6">
-            <img src={weatherData.icon} alt={weatherData.description} />
-            {Math.round(weatherData.temperature)}°C
-          </div>
-          <div className="col-6">
-            <ul>
-              <li>Precipitation: {weatherData.precipitation}%</li>
-              <li>Humidity: {weatherData.humidity}%</li>
-              <li>wind: {weatherData.wind}km/h</li>
-            </ul>
-          </div>
-        </div>
+        <WeatherInfo data={weatherData}/>
       </div>
     );
   } else {
