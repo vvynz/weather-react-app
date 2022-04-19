d19b230b1c2c03bcac8943fda3e2b5854c154b53
Vivian Zhang <vvynz12@gmail.com>
2022-04-19 18:32:31 -0400

    Move the code from SearchEngine

--- src/WeatherInfo.jsx
+++ src/WeatherInfo.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 import FormatDate from "./FormateDate";
 
-export default function WeatherInfo() {
+export default function WeatherInfo(props) {
   return (
-    <h1>HELLO
-    </h1>
+    <div>
+      <h1>{weatherData.city}</h1>
+        <FormatDate date={weatherData.date} />
+        <p>{weatherData.description}</p>
+        <div className="row">
+          <div className="col-6">
+            <img src={weatherData.icon} alt={weatherData.description} />
+            {Math.round(weatherData.temperature)}°C
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Precipitation: {weatherData.precipitation}%</li>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>wind: {weatherData.wind}km/h</li>
+            </ul>
+          </div>
+        </div>
+    </div>
   );
 }
