c4864f8bbe0bec87878d64e159e9ca6533921298
Vivian Zhang <vvynz12@gmail.com>
2022-04-19 19:06:06 -0400

    Create a iconMap function to match the icon codes from the OpenWeather api to the icon codes from the ReactAnimatedWeather dependency

--- src/WeatherIcon.jsx
+++ src/WeatherIcon.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactAnimatedWeather from "react-animated-weather";
 
 export default function WeatherIcon(props) {
+  const iconMap = {
+    "04d": "CLOUDY"
+  }
   return(
     <ReactAnimatedWeather 
-    icon="CLEAR_DAY"
+    icon={iconMap[props.icon]}
     color="PURPLE"
     size={60}
     animate={true}
