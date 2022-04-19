a579ca19b2bb93c02914d7635a04e1f57c84fdea
Vivian Zhang <vvynz12@gmail.com>
2022-04-19 18:59:23 -0400

    Pass test data to ReactAnimatedWeather to test if animated icons work

--- src/WeatherIcon.jsx
+++ src/WeatherIcon.jsx
@@ -4,6 +4,10 @@ import ReactAnimatedWeather from "react-animated-weather";
 export default function WeatherIcon(props) {
   return(
     <ReactAnimatedWeather 
+    icon="CLEAR_DAY"
+    color="PURPLE"
+    size={60}
+    animate={true}
     />
   );
 }
