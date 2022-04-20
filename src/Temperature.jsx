133ac1b47416dff777a3558a7e4b4fa5594ca4eb
Vivian Zhang <vvynz12@gmail.com>
2022-04-19 21:03:01 -0400

    Create a convertToF function and a state that will toggle between the units celsius and fahrenheit

--- src/Temperature.jsx
+++ src/Temperature.jsx
@@ -1,10 +1,31 @@
 import React, { useState } from "react";
 
 export default function Temperature(props) {
-  
-  return(
-    <div className="Temp">
-      <span>{Math.round(props.celsius)}°C | <a href="/" >°F</a></span>
-    </div>
-  );
+  const [unit, setUnit] = useState("celsius");
+
+  function convertToF(e) {
+    e.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  if (unit === "celsius") {
+    return (
+      <div className="Temp">
+        <span>
+          {Math.round(props.celsius)}°C |{" "}
+          <a href="/" onClick={convertToF}>
+            °F
+          </a>
+        </span>
+      </div>
+    );
+  } else {
+    return (
+      <div className="Temp">
+        <span>
+          {Math.round(props.celsius)} <a href="/">°C</a> | °F
+        </span>
+      </div>
+    );
+  }
 }
