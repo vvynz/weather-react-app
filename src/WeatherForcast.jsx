fbfda310dd576312e82057531f00a9bd1f34e7e1
Vivian Zhang <vvynz12@gmail.com>
2022-04-22 18:29:17 -0400

    Import WeatherIcon component and pass in temporary icon code for testing

--- src/WeatherForcast.jsx
+++ src/WeatherForcast.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForcast(props) {
-  return(
+  return (
     <div className="WeatherForecast">
       <div className="row">
         <div className="col">
           <div className="WeatherForecast-date">Fri</div>
+          <div className="WeatherForecast-icon">
+            <WeatherIcon icon="09d" /> 
+            {/* TEMP DATA, WILL NEED TO UPDATE LATER */}
+          </div>
           <span className="WeatherForecast-temp-max">15°</span>
           <span className="WeatherForecast-temp-min">10°</span>
         </div>
