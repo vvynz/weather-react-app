0cf8a39d3c8ce6409c770b155e36323f0cdddce5
Vivian Zhang <vvynz12@gmail.com>
2022-04-24 19:55:27 -0400

    Create a WeatherForecastDay component to display the upcoming forecast

--- src/WeatherForecast.jsx
+++ src/WeatherForecast.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import WeatherIcon from "./WeatherIcon";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
   const [loaded, setLoaded] = useState(false);
---
+++ src/WeatherForecastDay.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function WeatherForecastDay(props) {
+  return("This is the upcoming forecast");
+}
