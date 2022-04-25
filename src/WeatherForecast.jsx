3d3ef331bbe8b4ad63706d33d9c022401c053371
Vivian Zhang <vvynz12@gmail.com>
2022-04-24 22:08:21 -0400

    Move all forecast code into the WeatherForecastDay component

--- src/WeatherForecast.jsx
+++ src/WeatherForecast.jsx
@@ -12,31 +12,26 @@ export default function WeatherForecast(props) {
     setForecastData(res.data.daily);
     setLoaded(true);
   }
+  
 
-  if(loaded) {
+  // if(loaded) {
     return (
       <div className="WeatherForecast">
         <div className="row">
           <div className="col">
-            <div className="WeatherForecast-date">Fri</div>
-            <div className="WeatherForecast-icon">
-              <WeatherIcon icon="09d" /> 
-              {/* TEMP DATA, WILL NEED TO UPDATE LATER */}
-            </div>
-            <span className="WeatherForecast-temp-max">15°</span>
-            <span className="WeatherForecast-temp-min">10°</span>
+            <WeatherForecastDay />
           </div>
         </div>
       </div>
     );
-  } else {
-    let lon = props.coord.lon;
-    let lat = props.coord.lat;
-    const apiKey = "1f485c022e4af72c068b4973496c26cc";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  // } else {
+    // let lon = props.coord.lon;
+    // let lat = props.coord.lat;
+    // const apiKey = "1f485c022e4af72c068b4973496c26cc";
+    // let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
   
-    axios.get(apiUrl).then(handleResponse);
+    // axios.get(apiUrl).then(handleResponse);
 
-    return null;
-  }
+  //   return null;
+  // }
 }
--- src/WeatherForecastDay.jsx
+++ src/WeatherForecastDay.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForecastDay(props) {
-  return("This is the upcoming forecast");
+  return (
+    <div>
+      <div className="WeatherForecast-date">Fri</div>
+      <div className="WeatherForecast-icon">
+        <WeatherIcon icon="09d" />
+        {/* TEMP DATA, WILL NEED TO UPDATE LATER */}
+      </div>
+      <span className="WeatherForecast-temp-max">15°</span>
+      <span className="WeatherForecast-temp-min">10°</span>
+    </div>
+  );
 }
