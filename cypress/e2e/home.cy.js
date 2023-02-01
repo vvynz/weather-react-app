219fe393f13b5e7e43f496d7ecc24e0454c7f94b
Vivian Zhang <vvynz12@gmail.com>
2023-02-01 18:12:48 -0500

    Add tests that checks the app displays the user's city, current date and current weather

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -7,4 +7,16 @@ describe('main page', () => {
     cy.get("h1").contains("Weather App")
   })
   
+  context("Weather app body, info display", () => {
+    it("displays the user's city and current date", () => {
+      cy.get(".date-display").contains("Wednesday 17:42")
+      cy.get(".city-display").contains("Toronto, CA")
+    })
+
+    it.only("displays the current weather info", () => {
+      cy.get(".temp-display").contains("-5°C")
+      cy.get(".temp-display").find("a").click()
+      cy.get(".temp-display").find("a").contains("°C") // this one changes everytime...
+    })
+  })
 })
