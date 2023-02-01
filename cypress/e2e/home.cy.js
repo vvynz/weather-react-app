10a7a2eb9ed399bf01f3504623ff51ec6790d9e9
Vivian Zhang <vvynz12@gmail.com>
2023-02-01 17:39:15 -0500

    Refactor the test so that before each test, it visits the home page

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,10 +1,9 @@
-describe('home page', () => {
-  it("should visit", ()=> {
+describe('main page', () => {
+  beforeEach(()=> {
     cy.visit("http://localhost:3000")
   })
 
   it("the h1 contains the correct test", () => {
-    cy.visit("http://localhost:3000")
     cy.get("h1").contains("Weather App")
   })
   
