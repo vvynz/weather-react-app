17f8a6d65ce655b9e460533644ea75931ae47f09
Vivian Zhang <vvynz12@gmail.com>
2023-02-01 17:36:55 -0500

    write first test that checks the h1 on the main page

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -2,5 +2,10 @@ describe('home page', () => {
   it("should visit", ()=> {
     cy.visit("http://localhost:3000")
   })
+
+  it("the h1 contains the correct test", () => {
+    cy.visit("http://localhost:3000")
+    cy.get("h1").contains("Weather App")
+  })
   
 })
