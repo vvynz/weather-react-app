4747b00a458547eb6f10cdd94f538791cfeb43b9
Vivian Zhang <vvynz12@gmail.com>
2023-02-01 19:09:51 -0500

    add tests that checks if users can search the weather of a city

--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -3,4 +3,8 @@ describe('search form', () => {
     cy.visit("http://localhost:3000")
   })
 
+  it("allows users to search the weather of a city", () => {
+    cy.get(".form-control").type("seoul")
+    cy.get(".search-btn").eq(1).click()
+  })
 })
