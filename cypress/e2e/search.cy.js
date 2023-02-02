9005a228f7079149165d82074137fc01bdf6841a
Vivian Zhang <vvynz12@gmail.com>
2023-02-01 19:22:44 -0500

    Add an assertion that verifies the search was a success

--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -4,7 +4,9 @@ describe('search form', () => {
   })
 
   it("allows users to search the weather of a city", () => {
+    // happy path
     cy.get(".form-control").type("seoul")
     cy.get(".search-btn").eq(1).click()
+    cy.get(".city-display").contains("Seoul, KR")
   })
 })
