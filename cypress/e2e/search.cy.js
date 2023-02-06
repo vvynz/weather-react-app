9a61dec5285c723aa50ed8b0a759f22bfab11544
Vivian Zhang <vvynz12@gmail.com>
2023-02-06 17:37:21 -0500

    Organize the tests by context

--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -1,25 +1,33 @@
-describe('search form', () => {
+describe("search form", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:3000")
-  })
+    cy.visit("http://localhost:3000");
+  });
 
-  it("allows users to search the weather of a city", () => {
-    // happy path
-    // tests not always passing - due to api request error
-    cy.get(".form-control").type("seoul")
-    cy.get('.search-form > .row > :nth-child(3)').click()
-    cy.getByData("city-header").contains("Seoul, KR")
-  })
+  context("Search button", () => {
+    it("allows users to search the weather of a city", () => {
+      // happy path
+      // tests not always passing - due to api request error
+      cy.get(".form-control").type("seoul");
+      cy.get(".search-form > .row > :nth-child(3)").click();
+      cy.getByData("city-header").contains("Seoul, KR");
+    });
 
-  it("will display an error message if city isn't found", () => {
-    cy.get(".form-control").type("sappporro")
-    cy.get(".search-form > .row > :nth-child(3").click()
-    cy.getByData("error-display").should("exist")
-  })
+    it("will display an error message if city isn't found", () => {
+      cy.get(".form-control").type("sappporro");
+      cy.get(".search-form > .row > :nth-child(3").click();
+      cy.getByData("error-display").should("exist");
+    });
 
-  it("will remove the error message once a city is found", () => {
-    cy.get(".form-control").type("sapporo")
-    cy.get(".search-form > .row > :nth-child(3)").click()
-    cy.getByData("error-display").should("not.exist")
+    it("will remove the error message once a city is found", () => {
+      cy.get(".form-control").type("sapporo");
+      cy.get(".search-form > .row > :nth-child(3)").click();
+      cy.getByData("error-display").should("not.exist");
+    });
+  });
+
+  context("Get location button", () => {
+    it.only("will get the user's current location", () => {
+      
+    })
   })
-})
+});
